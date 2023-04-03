fb1d3019d3f15cfad4fcdf7666d00fc7ebccbfc9
ilana <ifl6325@rit.edu>
2023-04-03 19:12:29 -0400

    added deez nuts comment

--- app/src/main/java/com/example/idkapp/MainActivity.kt
+++ app/src/main/java/com/example/idkapp/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    //Goblin
+    // Goblin deez nuts
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
